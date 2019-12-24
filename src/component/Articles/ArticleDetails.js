e64fa9f756e370b171037334fe0ac12077d52db7
fengyunlsm <2399548030@qq.com>
2019-12-25 01:04:16 +0800

    新增文章分享和点赞

--- src/component/Articles/ArticleDetails.js
+++ src/component/Articles/ArticleDetails.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import {Row, Col, Comment, List, Input, Button, message} from 'antd';
+import {Row, Col, Comment, List, Input, Button, message, Modal, Icon} from 'antd';
 import { withRouter } from 'react-router-dom';
 import storage from "../storage";
 import noAuthor from "../../images/no-author.png";
 import ReactMarkdown from 'react-markdown';
-import { getArticleDetails, getArticleComments, postArticleComment } from '../../fetch'
+import { getArticleDetails, getArticleComments, postArticleComment,  getMyLikeArtilce, addLike, cancelLike} from '../../fetch';
+import QRCode  from 'qrcode.react';
 
 class ArticleDetails extends Component{
     constructor(props){
         super(props);
         this.state = {
             usercomment: '',
-            articleId: this.props.match.params.id
+            articleId: this.props.match.params.id,
+            myLikeArtilce: [],
+            like: {background: "#ccc5c5"}
         }
     }
 
@@ -19,16 +22,48 @@ class ArticleDetails extends Component{
         console.log(this.props.match.params.id);
         this.getArticleDetails(this.state.articleId);
         this.getArticleComments(this.state.articleId);
+        // 如果已经登录，则显示likeAct, 如果没有登录，则显示普通的图片
+        this.getMyLikeArtilce();
     };
 
     getArticleDetails = (id) => {
         getArticleDetails(id).then((res) => {
-            console.log(res)
+            console.log('文章内容: ', res)
             this.setState({
                 article: res[0]
             });
         })
     };
+
+    addLike = () => {
+      addLike(
+        {"articleId": this.state.articleId}
+      ).then((res) => {
+        console.log('对文章进行点赞')
+        this.setState({
+          like: {background: "#ff6e37"}
+        })
+        this.getArticleDetails(this.state.articleId)
+        // 重新获取我喜欢的文章
+        this.getMyLikeArtilce()
+      })
+    }
+
+    cancelLike = () => {
+      // 取消对文章的点赞
+      cancelLike(
+        {"articleId": this.state.articleId}
+      ).then((res) => {
+        console.log('取消对文章的点赞')
+        this.setState({
+          like: {background: "#ccc5c5"}
+        })
+        this.getArticleDetails(this.state.articleId)
+        // 重新获取我喜欢的文章
+        this.getMyLikeArtilce()
+
+      })
+    }
 
     getArticleComments = (id) => {
         getArticleComments(id).then((res) => {
@@ -38,6 +73,20 @@ class ArticleDetails extends Component{
             });
         })
     };
+
+    getMyLikeArtilce = () => {
+      if (storage.get('token')) {
+        // 获取我喜欢的文章  并 显示 点赞状态
+        getMyLikeArtilce().then((res) => {
+          console.log('我喜欢的文章: ', res)
+          this.setState({
+              myLikeArtilce: res
+          })
+        }).then ((res) => {
+          this.getLikePic(this.state.myLikeArtilce)
+        })
+      }
+    }
 
     onFieldChange = (e) => {
         this.setState({usercomment: e.target.value});
@@ -80,6 +129,63 @@ class ArticleDetails extends Component{
         }
     };
 
+
+    showShare = () => {
+      let path = window.location.href
+
+      Modal.info({
+          title: '打开微信“扫一扫”，打开网页后点击屏幕右上角分享按钮',
+          content: (
+              <div className='share-modal'>
+                  <QRCode
+                      value = {path}  //value参数为生成二维码的链接
+                      size = {100} //二维码的宽高尺寸
+                      fgColor = "#000000"  //二维码的颜色
+                 />
+              </div>
+          ),
+          onCancel() {},
+      });
+    };
+
+    Like = () => {
+      if (!storage.get('token')) {
+        message.error("请登陆后再点赞", 2);
+      } else {
+        if (this.isMyLike(this.state.myLikeArtilce)) {
+          console.log('取消点赞')
+          this.cancelLike()
+        } else {
+          console.log('点赞')
+          this.addLike()
+        }
+      }
+    };
+
+    isMyLike = (myLikeArtilce) => {
+      for (let item of myLikeArtilce) {
+        if (item.articleId === this.state.articleId) {
+          return true
+        }
+      }
+      return false
+    };
+
+    getLikePic = (myLikeArtilce) => {
+      // 判断是不是自己点赞的文章
+      if (this.isMyLike(myLikeArtilce)) {
+        console.log('确实是我喜欢')
+        this.setState({
+          like: {background: "#ff6e37"}
+        })
+      } else {
+        console.log('不是我喜欢的文章')
+        this.setState({
+          like: {background: "#ccc5c5"}
+        })
+      }
+    };
+
     render() {
         let article = this.state.article;
         return (
@@ -114,6 +220,18 @@ class ArticleDetails extends Component{
                                     <img src={this.state.headPortrait ? this.state.headPortrait : noAuthor} alt='' />
                                     <Input onChange={this.onFieldChange} value={this.state.usercomment} placeholder='说点什么...'/>
                                     <Button onClick={() => this.commentSubmit()}>评论</Button>
+                                    {
+                                        article &&
+                                        <Button icon="like" onClick={this.Like} style={this.state.like}>
+                                          <span class="text">点赞</span>
+                                          <span className="like-count">{article && article.praiseNumber}</span>
+                                        </Button>
+
+                                    }
+                                    <Button type="primary" onClick={this.showShare} class="share" style={{background: "#ccc5c5"}}>
+                                      <Icon type="share-alt" />
+                                      分享
+                                    </Button>
                                 </div>
                                 <div className='comment-list'>
                                     {
@@ -143,6 +261,7 @@ class ArticleDetails extends Component{
             </div>
         )
     }
+
 }
 
 export default withRouter(ArticleDetails);
